b4454a15a0dbd20dbf678b2258b39baac943a96f
B00ttl3 <mumair3825@gmail.com>
2024-10-22 22:42:48 +0500

    Learnt about the Uppercase pipe in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
+import { LowerCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    UpperCasePipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'This loaded dynamically';
+  title: string = 'angular app';
 
+  upperCase() {
+    this.title = this.title.toUpperCase();
+    // this is JavaScript way not angular way
+  }
 }
