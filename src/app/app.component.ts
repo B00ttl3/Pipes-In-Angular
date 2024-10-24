55e634c20edb1fc22b0180f6cec351ec386df281
B00ttl3 <mumair3825@gmail.com>
2024-10-24 22:44:16 +0500

    Learnt about percentage pipe in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { DecimalPipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DecimalPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
     UpperCasePipe,
     LowerCasePipe,
     TitleCasePipe,
-    DecimalPipe
+    DecimalPipe,
+    PercentPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
