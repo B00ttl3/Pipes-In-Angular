42215659c0d8e3822f09b49fcb5f460a92c91647
B00ttl3 <mumair3825@gmail.com>
2024-10-29 21:15:44 +0500

    Learnt about the JSON pipe of Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { CurrencyPipe, DatePipe, DecimalPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -11,6 +11,7 @@ import { Component } from '@angular/core';
     PercentPipe,
     CurrencyPipe,
     DatePipe,
+    JsonPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
