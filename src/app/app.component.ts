f0dc04f67c95096e67f0531d54f5466ccb3ffb4f
B00ttl3 <mumair3825@gmail.com>
2024-10-30 23:26:08 +0500

    Learnt about the slice pipe in Angular with which we can extrat the specific portion of some string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, LowerCasePipe, PercentPipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -11,7 +11,8 @@ import { Component } from '@angular/core';
     PercentPipe,
     CurrencyPipe,
     DatePipe,
-    JsonPipe
+    JsonPipe,
+    SlicePipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
